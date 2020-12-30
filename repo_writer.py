38f3a03ba40a8e387dcbeecdb46679112b604449
henrythier <henry@thierhoff.rocks>
2020-12-30 19:19:16 +0100

    added script to write data to repo

---
+++ repo_writer.py
@@ -0,0 +1,20 @@
+from github import Github
+import requests
+import json
+import os;
+
+token = os.getenv('token')
+repo_path = 'henrythier/vax_stats'
+
+# authenticate
+g = Github(token)
+
+def update_file(file_path, commit_message, string_content):
+    # get repo
+    repo = g.get_repo('henrythier/vax_stats')
+
+    # get sha
+    contents = repo.get_contents(file_path)
+
+    # update file
+    repo.update_file(contents.path, commit_message, string_content, contents.sha, branch="main")
